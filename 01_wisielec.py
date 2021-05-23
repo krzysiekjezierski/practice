297faf7009dd1aeb1d516f3e0d9a1f2af0c5c649
Krzychu-Lenovo <krisjarzyna@gmail.com>
2021-05-23 11:36:51 +0200

    "finished, to be improved"#
     Please enter the commit message for your changes. Lines starting

--- 01_wisielec.py
+++ 01_wisielec.py
@@ -1,8 +1,10 @@
+import sys
+
 number_of_tries = 5
-word = "kamil"
+word = "dupa wołowa"
 used_letters = []
 
-used_word = []
+user_word = []
 
 def find_indexes(word, letter):
     indexes = []
@@ -12,16 +14,41 @@ def find_indexes(word, letter):
             indexes.append(index)
 
     return indexes
+def show_state_of_game():
+    print()
+    print(user_word)
+    print("Pozostało prób:", number_of_tries)
+    print("użyte litery", used_letters)
+    print()
 
 ###
 
 for _ in word:
-    used_word.append('_')
+    user_word.append('_')
 
 while True:
     letter = input("Podaj literę: ")
     used_letters.append(letter)
 
-    print(find_indexes(word, letter))
+    found_indexes = find_indexes(word, letter)
 
-    print('Użyte litery:',used_letters)
+    if len(found_indexes) == 0:
+        print("\nNie ma tekiej litery.")
+        number_of_tries -= 1
+        # print("Pozostało prób", number_of_tries)
+
+        if number_of_tries == 0:
+            print("Koniec gry, nie udało się, wisisz ;-(")
+            sys.exit(0)
+    else:
+        for index in found_indexes:
+            user_word[index] = letter
+        # print(user_word)
+
+        if "".join(user_word) == word:
+            print("\nBrawo, udało Ci się tym razem ;-)")
+            sys.exit(0)
+
+    # print('Użyte litery:',used_letters)
+
+    show_state_of_game()
